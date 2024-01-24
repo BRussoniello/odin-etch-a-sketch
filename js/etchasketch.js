c2dbb3d1a1441cc038c65e7764bdf89b5d1f02b0
bcr <benjamin.russoniello@gmail.com>
2024-01-24 18:10:56 -0500

    modularize;add resize

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,41 +1,72 @@
 /*
-Grid size = # of boxes in container
+Grid size = # of boxes per container side
 */
 
 //user needs to be PROMPTED when clicking button. use prompt()
 //text field will default to 16.
-const INITIAL_GRID_SIZE = +(document.querySelector("#grid-size").textContent);
-let currentGridSize = INITIAL_GRID_SIZE;
-let squareContainer = document.querySelector(".square-container");
-let squareContainerSize = squareContainer.offsetWidth;
-const INITIAL_BOX_SIDE_SIZE = getBoxSideLength(squareContainerSize,INITIAL_GRID_SIZE);
 
-for (let i = 1; i <= INITIAL_GRID_SIZE; i++) {
-    let row = document.createElement("div");
-    row.setAttribute("class","row");
-    row.style.display="flex";
-    row.style.width = intToSizeStyleString(INITIAL_GRID_SIZE*INITIAL_BOX_SIDE_SIZE);
-    row.style.height = intToSizeStyleString(INITIAL_BOX_SIDE_SIZE);
-    for (let j = 1; j<= INITIAL_GRID_SIZE; j++) {
-        let rowSquare = document.createElement("div")
-        rowSquare.setAttribute("class","row-square");
-        rowSquare.style.height = intToSizeStyleString(INITIAL_BOX_SIDE_SIZE);
-        rowSquare.style.width = intToSizeStyleString(INITIAL_BOX_SIDE_SIZE);
-        rowSquare.style.border = "1px solid black";
-        rowSquare.style.boxSizing="border-box";
-        //add mouse hover event listener
-        row.appendChild(rowSquare);
-        
-        
+function makeGrid(gridSize,gridContainer,boxSize) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild)
     }
-    squareContainer.appendChild(row)
-}
+    for (let i = 1; i <= gridSize; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("class","row");
+        row.style.display="flex";
+        row.style.width = intToSizeStyleString(gridSize*boxSize);
+        row.style.height = intToSizeStyleString(boxSize);
+        
+        for (let j = 1; j<= gridSize; j++) {
+            let rowSquare = document.createElement("div")
+            rowSquare.setAttribute("class","row-square");
+            rowSquare.style.height = row.style.height;
+            rowSquare.style.width = row.style.height;
+            rowSquare.style.border = "1px solid black";
+            rowSquare.style.boxSizing="border-box";
+            //add mouse hover event listener
+            row.appendChild(rowSquare);
+            
+            
+            }
+            gridContainer.appendChild(row)
+        }
+    }
 
 function getBoxSideLength(containerSideLength,grid_size) {
     return containerSideLength/grid_size;
-}
+    }
 
 function intToSizeStyleString(int) {
     return int + "px";
+    }
+
+function gridSizeIsValid(gridSize) {
+    return gridSize <= 100 && gridSize > 0;
 }
 
+function main() {
+    const gridSizeSpan = document.querySelector("#grid-size");
+    const gridSizeButton = document.querySelector(".size-button");
+    const INITIAL_GRID_SIZE = +(gridSizeSpan.textContent);
+    let currentGridSize = INITIAL_GRID_SIZE;
+    let squareContainer = document.querySelector(".square-container");
+    let squareContainerSize = squareContainer.offsetWidth;
+    const INITIAL_BOX_SIDE_SIZE = getBoxSideLength(squareContainerSize,INITIAL_GRID_SIZE);
+
+    gridSizeButton.addEventListener("click", (e) => {
+        const newGridSize = +prompt("Enter a new grid size (1-100):");
+        if (gridSizeIsValid(newGridSize)) {
+            const newBoxSideSize = getBoxSideLength(squareContainerSize, newGridSize);
+            makeGrid(newGridSize,squareContainer,newBoxSideSize);
+            gridSizeSpan.textContent = newGridSize;
+        }
+        else {
+            alert("Invalid grid size.");
+    
+            }
+        }
+    )
+    makeGrid(currentGridSize,squareContainer,INITIAL_BOX_SIDE_SIZE);
+}
+
+main();
